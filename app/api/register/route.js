e1e085fcbdcfef58e3ab2ed09a4654643c5069f4
Ziming Yuan <zimingyuan2017@gmail.com>
2023-08-21 07:43:32 +0800

    db:user registration

---
+++ app/api/register/route.js
@@ -0,0 +1,16 @@
+import { connectMongoDB } from "../../lib/mongodb";
+import User from "../../models/user";
+import { NextResponse } from "next/server";
+import bcrypt from "bcryptjs";
+
+export async function POST(req) {
+    try {
+        const {email, password} = await req.json();
+        const hashedPassword = await bcrypt.hash(password, 10);
+        await connectMongoDB();
+        await User.create({ email, password: hashedPassword });
+        return NextResponse.json({message: "user registered"}, {status: 201});
+    } catch {
+        return NextResponse.json({message: "error while registering"}, {status: 500})
+    }
+}
--- app/register/page.jsx
+++ app/register/page.jsx
@@ -7,6 +7,34 @@ export default function Home() {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [error, setError] = useState("");
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        if (!email || !password) {
+            setError("All fields are necessary");
+            return;
+        }
+        try {
+            const res = await fetch("api/register", {
+                method: "POST",
+                headers: {
+                  "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                  email,
+                  password,
+                }),
+            });
+            if (res.ok) {
+                const form = e.target;
+                form.reset();
+                router.push("/");
+            } else {
+                console.log("User registration failed.");
+            }
+        } catch (error) {
+            console.log("Error during registration: ", error);
+        }
+    }
 
   return (
     <main className="flex justify-center items-center min-h-screen">
@@ -24,20 +52,36 @@ export default function Home() {
           </p>
           
           {/* Form */}
-          <form>
+          <form onSubmit={handleSubmit}>
               <div className="mb-4">
                   <label htmlFor="email" className="block text-sm font-medium leading-tight text-slate-800 mb-2">Email Address</label>
-                  <input type="email" id="email" name="email" placeholder="Enter your email" className="w-full h-9 px-2 py-3 bg-white rounded border border-gray-300" />
+                  <input
+                    onChange={e=>setEmail(e.target.value)}
+                    type="email"
+                    id="email"
+                    name="email"
+                    placeholder="Enter your email"
+                    className="w-full h-9 px-2 py-3 bg-white rounded border border-gray-300" />
               </div>
               
               <div className="mb-4 relative">
                   <label htmlFor="password" className="block text-sm font-medium leading-tight text-slate-800 mb-2">Password</label>
-                  <input type="password" id="password" name="password" placeholder="Enter your password" className="w-full h-9 px-2 py-3 bg-white rounded border border-gray-300" />
+                  <input
+                    onChange={e=>setPassword(e.target.value)}
+                    type="password"
+                    id="password"
+                    name="password"
+                    placeholder="Enter your password"
+                    className="w-full h-9 px-2 py-3 bg-white rounded border border-gray-300" />
               </div>
 
-              <div>
-                  <button type="submit" className="w-full h-9 bg-indigo-600 rounded text-white font-medium mb-2">Register</button>
-              </div>
+              <button type="submit" className="w-full h-9 bg-indigo-600 rounded text-white font-medium mb-2">Register</button>
+
+              {error && (
+                <div className="bg-red-500 text-white w-fit text-sm py-1 px-3 rounded-md mt-2">
+                {error}
+                </div>
+               )}
 
               <div className="text-sm mt-3 text-right text-gray-900">
                 Already have an account? <Link href={"/"} className="underline">Login</Link>

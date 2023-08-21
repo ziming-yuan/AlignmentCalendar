5f8bef409f4d5a8fd0469cb8eb0fff02afad66b6
Ziming Yuan <zimingyuan2017@gmail.com>
2023-08-21 12:56:33 +0800

    auth: signout

---
+++ app/dashboard/page.jsx
@@ -0,0 +1,12 @@
+import Navbar from '../components/Navbar.jsx';
+
+function HomePage() {
+  return (
+    <div>
+      <Navbar />
+      {/* Rest of your page content */}
+    </div>
+  );
+}
+
+export default HomePage;

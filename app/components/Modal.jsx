1aae7445aec6a6528c129b4241a87eacc876c7ac
Ziming Yuan <zimingyuan2017@gmail.com>
2023-08-22 09:12:22 +0800

    frontend: modal template

---
+++ app/components/Modal.jsx
@@ -0,0 +1,89 @@
+'use client'
+import { Fragment, useRef } from 'react'
+import { Dialog, Transition } from '@headlessui/react'
+
+export default function Modal({ isOpen = false, setIsOpen = () => {}, title, body, confirmLabel, onConfirm }) {
+  const cancelButtonRef = useRef(null)
+
+  return (
+    <Transition.Root show={isOpen} as={Fragment}>
+      <Dialog as="div" className="relative z-10" initialFocus={cancelButtonRef} onClose={setIsOpen}>
+        {/* Transition effects - bg grayed out */}
+        <Transition.Child
+          as={Fragment}
+          enter="ease-out duration-300"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="ease-in duration-200"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
+        >
+          <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+        </Transition.Child>
+
+        <div className="fixed inset-0 z-10 overflow-y-auto">
+          <div className="flex min-h-full items-end justify-center p-4 text-center items-center sm:p-0">
+            {/* Fades in from being transparent and slightly shifted downwards (or scaled down on small screens) to fully opaque and in its natural position (and size).
+            Fades out by becoming transparent again and shifting slightly downwards (or scaling down on small screens). */}
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+              enterTo="opacity-100 translate-y-0 sm:scale-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+              leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+            >
+              <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
+                {/* Modal header */}
+                <div className="border-b border-gray-300 px-4 pt-4 pb-3">
+                    <div className="text-center sm:ml-4 sm:text-left">
+                    <Dialog.Title as="h3" className="text-base font-semibold leading-6 text-gray-900">
+                        {title}
+                    </Dialog.Title>
+                    </div>
+                </div>
+
+                {/* Modal body */}
+                <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
+                    <div className="sm:flex sm:items-start">
+                    <div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
+                        {/* <div className="mt-2">
+                        <p className="text-sm text-gray-500">
+                            Are you sure you want to deactivate your account? All of your data will be permanently removed. This action cannot be undone.
+                        </p>
+                        </div> */}
+                        {body}
+                    </div>
+                    </div>
+                </div>
+
+                {/* Modal footer */}
+                <div className="border-t border-gray-300 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
+                  <button
+                    type="button"
+                    className="inline-flex w-full justify-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 sm:ml-3 sm:w-auto"
+                    onClick={() => {
+                        onConfirm();
+                        setIsOpen(false);
+                    }}
+                  >
+                    {confirmLabel}
+                  </button>
+                  <button
+                    type="button"
+                    className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-indigo-300 hover:bg-indigo-50 sm:mt-0 sm:w-auto"
+                    onClick={() => setIsOpen(false)}
+                    ref={cancelButtonRef}
+                  >
+                    Cancel
+                  </button>
+                </div>
+              </Dialog.Panel>
+            </Transition.Child>
+          </div>
+        </div>
+      </Dialog>
+    </Transition.Root>
+  )
+}

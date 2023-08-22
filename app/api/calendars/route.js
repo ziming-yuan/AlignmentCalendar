52824a57ac0aa4e265e8f0fb67e3b9d0a21356c1
Ziming Yuan <zimingyuan2017@gmail.com>
2023-08-22 16:11:49 +0800

    api: POST new calendar using user email

--- app/api/calendars/route.js
+++ app/api/calendars/route.js
@@ -10,3 +10,48 @@ export async function DELETE(req) {
     await Calendar.findByIdAndDelete(calendarId);
     return NextResponse.json({ message: "Deleted successfully!" });
 }
+
+// Create a new calendar
+// email and title required
+export async function POST(req) {
+  try {
+    await dbConnect();
+    const { 
+      email,
+      title,
+      description,
+      isActive,
+      titleTextColor,
+      titleBackgroundColor,
+      backgroundPhoto,
+      logoImage,
+      doors 
+    } = await req.json();
+    console.log("email:", email)
+    console.log("title:", title)
+    console.log("des:", description)
+    // Find the user by email
+    const user = await User.findOne({ email }).select("_id");
+    // If user is not found, return an error response
+    if (!user) {
+      return NextResponse.json({message: "error: user not found"}, {status: 400});
+    }
+    const newCalendar = new Calendar({
+      owner: user._id,
+      title,
+      description,
+      isActive,
+      titleTextColor,
+      titleBackgroundColor,
+      backgroundPhoto,
+      logoImage,
+      doors
+    });
+    await Calendar.create(newCalendar);
+    // Return a success response
+    return NextResponse.json({message: "Calendar created successfully!"}, {status: 201});
+  } catch (error) {
+    console.error("Error creating calendar:", error);
+    return NextResponse.json({message: "error while creating a calendar"}, {status: 500})
+  }
+}

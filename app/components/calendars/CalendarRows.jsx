552af1f0305ea7c85818d33d9b3d8ea85f034d64
Ziming Yuan <zimingyuan2017@gmail.com>
2023-08-26 22:41:42 +0800

    frontend: removed refetch context

--- app/components/calendars/CalendarRows.jsx
+++ app/components/calendars/CalendarRows.jsx
@@ -1,29 +1,19 @@
 'use client';
 import React, { useState, useEffect, useContext } from "react";
 import { useSession } from "next-auth/react";
-import RouterContext from "../contextProviders/RouterContext";
-import RefetchContext from "../contextProviders/RefetchContext";
 import ActiveButton from "./ActiveButton";
 import InactiveButton from "./InactiveButton";
 import {PencilIcon, EyeIcon, CloudArrowUpIcon, CloudArrowDownIcon, ShareIcon, CodeBracketIcon, TrashIcon} from "@heroicons/react/24/outline";
 
 export default function CalendarRows() {
   const [calendars, setCalendars] = useState([]);
-  // const [refetch, setRefetch] = useState(false);
   const { data: session } = useSession();
-  const {router} = useContext(RouterContext);
 
   useEffect(() => {
     if (session?.user?.email) {
       fetchCalendars(session.user.id);
     }
   }, [session]);
-
-  // useEffect(() => {
-  //   if (session?.user?.id){
-  //     fetchCalendars(session.user.id);
-  //   }
-  // }, [refetch])
 
   async function fetchCalendars(id) {
     try {
@@ -88,69 +78,61 @@ export default function CalendarRows() {
   }
 
   return (
-    <RefetchContext.Provider value={{onRefetch: () => {
-        // setRefetch(!refetch);
-        // console.log("refetch value", refetch);
-        console.log("about to fetch Calendars")
-        fetchCalendars(session?.user.id)
-      }}}>
-      <tbody>
-      {calendars.map(calendar => (
-        <tr key={calendar._id} className="border-b text-sm">
-          <td className="px-4 py-2">{calendar.title}</td>
-          <td className="px-4 py-2">{calendar.description}</td>
-          <td className="px-4 py-2">{calendar.isActive ? <ActiveButton/> : <InactiveButton/>}</td>
-          <td className="px-4 py-2">
-            <button
-              className="text-indigo-700 px-2 py-1 m-1 inline-flex items-center border-gray-100 border shadow rounded-md hover:bg-indigo-600 hover:text-white hover:border-gray-300 transition duration-150 ease-in-out"
-            >
-              Edit
-              <PencilIcon className="w-3 h-3 ml-1"/>
-            </button>
-            <button 
-              className="text-indigo-700 px-2 py-1 m-1 inline-flex items-center border-gray-100 border shadow rounded-md hover:bg-indigo-600 hover:text-white hover:border-gray-300 transition duration-150 ease-in-out"
-            >
-              View 
-              <EyeIcon  className="w-4 h-4 ml-1"/>
-            </button>
-            <button
-              onClick={() => handlePost(calendar._id)}
-              className="text-indigo-700 px-2 py-1 m-1 inline-flex items-center border-gray-100 border shadow rounded-md hover:bg-indigo-600 hover:text-white hover:border-gray-300 transition duration-150 ease-in-out"
-            >
-              Post
-              <CloudArrowUpIcon  className="w-4 h-4 ml-1"/>
-            </button>
-            <button
-              onClick={() => handleHide(calendar._id)}
-              className="text-indigo-700 px-2 py-1 m-1 inline-flex items-center border-gray-100 border shadow rounded-md hover:bg-indigo-600 hover:text-white hover:border-gray-300 transition duration-150 ease-in-out"
-            >
-              Hide
-              <CloudArrowDownIcon  className="w-4 h-4 ml-1"/>
-            </button>
-            <button
-              className="text-indigo-700 px-2 py-1 m-1 inline-flex items-center border-gray-100 border shadow rounded-md hover:bg-indigo-600 hover:text-white hover:border-gray-300 transition duration-150 ease-in-out"
-            >
-              Share
-              <ShareIcon  className="w-3.5 h-3.5 ml-1"/>
-            </button>
-            <button
-              className="text-indigo-700 px-2 py-1 m-1 inline-flex items-center border-gray-100 border shadow rounded-md hover:bg-indigo-600 hover:text-white hover:border-gray-300 transition duration-150 ease-in-out"
-            >
-              Code
-              <CodeBracketIcon  className="w-4 h-4 ml-1"/>
-            </button>
-            <button 
-              onClick={() => handleDelete(calendar._id)} 
-              className="text-indigo-700 px-2 py-1 m-1 inline-flex items-center border-gray-100 border shadow rounded-md hover:bg-indigo-600 hover:text-white hover:border-gray-300 transition duration-150 ease-in-out"
-            >
-              Delete
-              <TrashIcon  className="w-3.5 h-3.5 ml-1"/>
-            </button>
-          </td>
-        </tr>
-      ))}
-      </tbody>
-    </RefetchContext.Provider>
-
+    <tbody>
+    {calendars.map(calendar => (
+      <tr key={calendar._id} className="border-b text-sm">
+        <td className="px-4 py-2">{calendar.title}</td>
+        <td className="px-4 py-2">{calendar.description}</td>
+        <td className="px-4 py-2">{calendar.isActive ? <ActiveButton/> : <InactiveButton/>}</td>
+        <td className="px-4 py-2">
+          <button
+            className="text-indigo-700 px-2 py-1 m-1 inline-flex items-center border-gray-100 border shadow rounded-md hover:bg-indigo-600 hover:text-white hover:border-gray-300 transition duration-150 ease-in-out"
+          >
+            Edit
+            <PencilIcon className="w-3 h-3 ml-1"/>
+          </button>
+          <button 
+            className="text-indigo-700 px-2 py-1 m-1 inline-flex items-center border-gray-100 border shadow rounded-md hover:bg-indigo-600 hover:text-white hover:border-gray-300 transition duration-150 ease-in-out"
+          >
+            View 
+            <EyeIcon  className="w-4 h-4 ml-1"/>
+          </button>
+          <button
+            onClick={() => handlePost(calendar._id)}
+            className="text-indigo-700 px-2 py-1 m-1 inline-flex items-center border-gray-100 border shadow rounded-md hover:bg-indigo-600 hover:text-white hover:border-gray-300 transition duration-150 ease-in-out"
+          >
+            Post
+            <CloudArrowUpIcon  className="w-4 h-4 ml-1"/>
+          </button>
+          <button
+            onClick={() => handleHide(calendar._id)}
+            className="text-indigo-700 px-2 py-1 m-1 inline-flex items-center border-gray-100 border shadow rounded-md hover:bg-indigo-600 hover:text-white hover:border-gray-300 transition duration-150 ease-in-out"
+          >
+            Hide
+            <CloudArrowDownIcon  className="w-4 h-4 ml-1"/>
+          </button>
+          <button
+            className="text-indigo-700 px-2 py-1 m-1 inline-flex items-center border-gray-100 border shadow rounded-md hover:bg-indigo-600 hover:text-white hover:border-gray-300 transition duration-150 ease-in-out"
+          >
+            Share
+            <ShareIcon  className="w-3.5 h-3.5 ml-1"/>
+          </button>
+          <button
+            className="text-indigo-700 px-2 py-1 m-1 inline-flex items-center border-gray-100 border shadow rounded-md hover:bg-indigo-600 hover:text-white hover:border-gray-300 transition duration-150 ease-in-out"
+          >
+            Code
+            <CodeBracketIcon  className="w-4 h-4 ml-1"/>
+          </button>
+          <button 
+            onClick={() => handleDelete(calendar._id)} 
+            className="text-indigo-700 px-2 py-1 m-1 inline-flex items-center border-gray-100 border shadow rounded-md hover:bg-indigo-600 hover:text-white hover:border-gray-300 transition duration-150 ease-in-out"
+          >
+            Delete
+            <TrashIcon  className="w-3.5 h-3.5 ml-1"/>
+          </button>
+        </td>
+      </tr>
+    ))}
+    </tbody>
   );
 };
--- app/components/contextProviders/RefetchContext.js
+++
@@ -1,9 +0,0 @@
-import { createContext } from 'react';
-
-const defaultContextValue = {
-    onRefetch: () => {},
-};
-
-const RefetchContext = createContext(defaultContextValue);
-
-export default RefetchContext;

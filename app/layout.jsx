bb3f4001cea11da774c662593b3bb8ee27e1adcf
Ziming Yuan <zimingyuan2017@gmail.com>
2023-09-08 10:03:21 -0400

    favicon

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,20 +1,22 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
-import { AuthProvider } from "../components/Providers"
+import "./globals.css";
+import { Inter } from "next/font/google";
+import { AuthProvider } from "../components/Providers";
+import Favicon from "/public/favicon.ico";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Advent Calendar',
-  description: 'Advent-styled calendar for Interfaith Alignment',
-}
+    title: "Advent Calendar",
+    description: "Advent-styled calendar for Interfaith Alignment",
+    icons: [{ rel: "icon", url: Favicon.src }],
+};
 
 export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>
-        <AuthProvider>{children}</AuthProvider>
-      </body>
-    </html>
-  )
+    return (
+        <html lang="en">
+            <body className={inter.className}>
+                <AuthProvider>{children}</AuthProvider>
+            </body>
+        </html>
+    );
 }

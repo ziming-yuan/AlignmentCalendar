9fdeaf622c7f6479c984c2e2279777a613c61065
Ziming Yuan <zimingyuan2017@gmail.com>
2023-09-11 10:39:25 -0400

    testing: catch warning

--- app/_actions.js
+++ app/_actions.js
@@ -6,46 +6,49 @@ import Door from "/models/door";
 
 export async function updateDoorContent(fileData, formData, door) {
     "use server";
-    try {
-        const file = fileData.get("file");
-        const doorId = door._id;
-        const closedDoorText = formData.closedDoorText;
-        const date = formData.date;
-        const message = formData.message;
-        const youtubeVideoUrl = formData.youtubeVideoUrl;
-        const isFileUpdate = formData.isFileUpdate;
-        const deleteOgFile = formData.deleteOgFile;
-        const ogImageFileKey = door.contentImage.fileKey;
-        await dbConnect();
+    const file = fileData.get("file");
+    const doorId = door._id;
+    const closedDoorText = formData.closedDoorText;
+    const date = formData.date;
+    const message = formData.message;
+    const youtubeVideoUrl = formData.youtubeVideoUrl;
+    const isFileUpdate = formData.isFileUpdate;
+    const deleteOgFile = formData.deleteOgFile;
+    const ogImageFileKey = door.contentImage.fileKey;
+    await dbConnect();
 
-        await Door.findByIdAndUpdate(doorId, {
-            date: new Date(date),
-            closedDoorText,
-            message,
-            youtubeVideoUrl,
-        });
+    await Door.findByIdAndUpdate(doorId, {
+        date: new Date(date),
+        closedDoorText,
+        message,
+        youtubeVideoUrl,
+    });
 
-        if (deleteOgFile) {
-            console.log("inside delete");
-            console.log(ogImageFileKey);
+    if (deleteOgFile) {
+        console.log("inside delete");
+        console.log(ogImageFileKey);
 
+        try {
             // delete the previous file from uploadthing
             await utapi.deleteFiles(ogImageFileKey);
-
-            // delete the url in the database
-            await Door.findByIdAndUpdate(doorId, {
-                $set: {
-                    "contentImage.fileUrl": "",
-                    "contentImage.fileKey": "",
-                },
-            });
+        } catch (e) {
+            console.log("delete uploadthing error", e);
         }
 
-        if (isFileUpdate) {
-            console.log("inside update");
+        // delete the url in the database
+        await Door.findByIdAndUpdate(doorId, {
+            $set: {
+                "contentImage.fileUrl": "",
+                "contentImage.fileKey": "",
+            },
+        });
+    }
+
+    if (isFileUpdate) {
+        console.log("inside update");
+        try {
             // upload the new file to uploadthing
             const { data } = await utapi.uploadFiles(file);
-
             // update contentImage in the database
             await Door.findByIdAndUpdate(doorId, {
                 $set: {
@@ -53,10 +56,9 @@ export async function updateDoorContent(fileData, formData, door) {
                     "contentImage.fileKey": data.key,
                 },
             });
+        } catch (e) {
+            console.log("update error", e);
         }
-        revalidateTag("editPageDoors");
-    } catch (error) {
-        console.error("Error updating door content:", error);
-        throw error;
     }
+    revalidateTag("editPageDoors");
 }

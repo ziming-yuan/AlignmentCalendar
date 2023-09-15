74330d3dc91944426a3aaad9cbe37a95c60a2a3b
Ziming Yuan <zimingyuan2017@gmail.com>
2023-09-14 23:22:28 -0400

    auth: protect routes expect /calendar

--- models/calendar.js
+++ models/calendar.js
@@ -1,23 +1,27 @@
 import mongoose, { Schema, models } from "mongoose";
 
 const calendarSchema = new Schema({
-    owner: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-    path: { type: String, required: true },
+    owner: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+    },
+    path: { type: String, required: true, unique: true },
     title: { type: String, required: true },
     description: { type: String },
     isActive: {
         type: Boolean,
-        default: false
+        default: false,
     },
-    titleTextColor: { type: String, default: '#000000' },
+    titleTextColor: { type: String, default: "#000000" },
     backgroundImage: {
         fileUrl: { type: String },
-        fileKey: { type: String }
+        fileKey: { type: String },
     },
-    backgroundColor: { type: String, default: '#FFFFFF' },
+    backgroundColor: { type: String, default: "#FFFFFF" },
     logoImage: {
         fileUrl: { type: String },
-        fileKey: { type: String }
+        fileKey: { type: String },
     },
 });
 

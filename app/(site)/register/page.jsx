597c93c1151df81b09dd2a05c31cfeeee5c9099e
Ziming Yuan <zimingyuan2017@gmail.com>
2023-09-16 21:48:08 -0400

    register: fix register redirect

--- app/(site)/register/page.jsx
+++ app/(site)/register/page.jsx
@@ -33,7 +33,7 @@ export default function Register() {
         if (success) {
             setAccountCreated(true);
             setTimeout(() => {
-                router.push("dashboard");
+                router.push("/");
             }, 2000);
         } else {
             setError(error);

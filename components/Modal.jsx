18dc23c9cacce8c9bfc5b25fed3e929c56ef90bb
Ziming Yuan <zimingyuan2017@gmail.com>
2023-09-16 22:05:40 -0400

    frontend: modal submitting label

--- components/Modal.jsx
+++ components/Modal.jsx
@@ -84,13 +84,20 @@ export default function Modal({
                                         }`}
                                         disabled={isConfirmButtonDisabled}
                                     >
-                                        {confirmLabel}
+                                        {isConfirmButtonDisabled
+                                            ? "Submitting"
+                                            : confirmLabel}
                                     </button>
                                     <button
                                         type="button"
-                                        className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm text-gray-900 shadow-sm ring-1 ring-inset ring-indigo-300 hover:bg-indigo-50 sm:mt-0 sm:w-auto"
+                                        className={`mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm text-gray-900 shadow-sm ring-1 ring-inset ring-indigo-300 sm:mt-0 sm:w-auto ${
+                                            isConfirmButtonDisabled
+                                                ? "cursor-not-allowed opacity-50"
+                                                : "hover:bg-indigo-50"
+                                        }`}
                                         onClick={() => setIsModalOpen(false)}
                                         ref={cancelButtonRef}
+                                        disabled={isConfirmButtonDisabled}
                                     >
                                         Cancel
                                     </button>

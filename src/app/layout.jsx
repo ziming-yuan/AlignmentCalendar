19102dd54daa464c3b3c8000d05a95d182c42e63
Ziming Yuan <zimingyuan2017@gmail.com>
2023-10-23 01:00:47 -0400

    analytics: google analytics and cookie banner

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,7 +1,9 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
-import { AuthProvider } from "../components/Providers";
+import { AuthProvider } from "@/components/Providers";
 import { Analytics } from "@vercel/analytics/react";
+import GoogleAnalytics from "@/components/analytics/GoogleAnalytics";
+import CookieBanner from "@/components/analytics/cookieBanner";
 import Favicon from "/public/favicon.ico";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -16,9 +18,13 @@ export const metadata = {
 export default function RootLayout({ children }) {
     return (
         <html lang="en">
+            <GoogleAnalytics
+                GA_MEASUREMENT_ID={process.env.GA_MEASUREMENT_ID}
+            />
             <body className={inter.className}>
                 <AuthProvider>{children}</AuthProvider>
                 <Analytics />
+                <CookieBanner />
             </body>
         </html>
     );
---
+++ src/components/analytics/CookieBanner.jsx
@@ -0,0 +1,57 @@
+"use client";
+
+import Link from "next/link";
+import { getLocalStorage, setLocalStorage } from "@/lib/storageHelper";
+import { useState, useEffect } from "react";
+
+export default function CookieBanner() {
+    const [cookieConsent, setCookieConsent] = useState(false);
+
+    useEffect(() => {
+        const storedCookieConsent = getLocalStorage("cookie_consent", null);
+        setCookieConsent(storedCookieConsent);
+    }, [setCookieConsent]);
+
+    useEffect(() => {
+        const newValue = cookieConsent ? "granted" : "denied";
+        window.gtag("consent", "update", {
+            analytics_storage: newValue,
+        });
+        setLocalStorage("cookie_consent", cookieConsent);
+    }, [cookieConsent]);
+
+    return (
+        <div
+            className={`my-10 mx-auto max-w-max md:max-w-screen-xs fixed bottom-0 left-0 right-0 ${
+                cookieConsent === null ? "flex" : "hidden"
+            } px-3 md:px-4 py-3 justify-between items-center flex-col sm:flex-row gap-y-2 gap-x-4 rounded-lg shadow-lg`}
+        >
+            <div className="text-center text-sm">
+                <Link href="/info/cookies">
+                    <p className="inline-flex items-center">
+                        We use
+                        <span className="font-bold text-indigo-400 ml-1 mr-1">
+                            cookies
+                        </span>
+                        on our site.
+                    </p>
+                </Link>
+            </div>
+
+            <div className="flex gap-2">
+                <button
+                    className="text-sm text-indigo-700 px-2 py-1 inline-flex items-center border-gray-100 border shadow rounded-md hover:bg-indigo-600 hover:text-white hover:border-gray-300 transition duration-150 ease-in-out"
+                    onClick={() => setCookieConsent(false)}
+                >
+                    Decline
+                </button>
+                <button
+                    className="text-sm text-indigo-700 px-2 py-1 inline-flex items-center border-gray-100 border shadow rounded-md hover:bg-indigo-600 hover:text-white hover:border-gray-300 transition duration-150 ease-in-out"
+                    onClick={() => setCookieConsent(true)}
+                >
+                    Allow Cookies
+                </button>
+            </div>
+        </div>
+    );
+}
